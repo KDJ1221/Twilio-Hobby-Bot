690ac53e3a478629b2839fc3b885634b77eb3ba3
KDJ1221 <kimmikojames@gmail.com>
2020-04-30 00:46:08 -0700

    Setting up base code

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingReponse
 
 app = Flask(_name_)
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    #webhook
+    incoming_msg = request.values.get('Body', '').lower()
+
+    resp = MessagingReponse()
+    msg = resp.message()
+    msg.body('this is the response text')
+    msg.media(image_url)
+
+    responded = False
+    if 'quote' in incoming_msg:
+        
+        responded = True
+    
+    if 'cat' in incoming_msg:
+
+        responded = True
+
+    if not responded:
+        return "Generic response"
